f9672bb2ea3dddf59e5208c78b025a257c3d9b89
matheusucupira <matheus.sucupira@hotmail.com>
2023-11-15 10:35:25 -0300

    Calculadora de Partidas Rankeadas

---
+++ index.js
@@ -0,0 +1,24 @@
+function somarPartidas(wins, loses) {
+    return wins - loses
+}
+
+function wins() {
+    let resultWins = somarPartidas(345, 101)
+    if (resultWins < 11) {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [FERRO]`)
+    }else if (resultWins > 10 & resultWins < 21) {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [BRONZE]`)
+    }else if (resultWins > 20 & resultWins < 51) {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [PRATA]`)
+    }else if (resultWins > 50 & resultWins < 81) {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [OURO]`)
+    }else if (resultWins > 80 & resultWins < 91) {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [DIAMANTE]`)
+    }else if (resultWins > 90 & resultWins < 101) {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [LENDÁRIO]`)
+    }else {
+        console.log(`O Herói tem ${resultWins} vitórias e está no nível [IMORTAL]`)
+    }
+}
+
+wins()
